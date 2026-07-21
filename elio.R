7c95918680a73bd44b8cd6597397e360f835e0a3
Elio Campitelli <eliocampitelli@gmail.com>
2026-07-21 18:52:29 +1000

    Adds my tree

---
+++ elio.R
@@ -0,0 +1,26 @@
+# install.packages('flametree')
+library(flametree)
+
+# your name, without spaces or special character
+name <- "elio"
+
+# pick a seed
+this_seed <- 230756
+
+# pick some colours
+shades <- c("black", "violet", "purple", "yellow")
+
+# data structure defining the trees
+dat <- flametree_grow(seed = this_seed, time = 10, trees = 10)
+
+# draw the plot
+tree <- dat %>%
+  flametree_plot(
+    background = "antiquewhite",
+    palette = shades,
+    style = "nativeflora"
+  )
+
+tree
+# save the plot
+flametree_save(tree, filename = paste0("fig/tree_", name, ".png"))
